15281b4fcdcb744079214a7164a8a20f4a0b7ff7
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2023-05-29 11:50:25 -0600

    Continuar con los ejercicios del capitulo 3, antes del subtema 3.5

--- Part1RubyFoundations/chapter_three.rb
+++ Part1RubyFoundations/chapter_three.rb
@@ -87,3 +87,135 @@ puts "The first is for a #{th.venue} event on #{th.date}."
 puts "The second is for a #{cc.venue} event on #{cc.date}."
 
 # Setter methods
+=begin
+def discount(percent)
+  @price = @price *( 100 - percent) /100.0
+end
+=end
+
+#The equal sign (=) in method names
+class Ticket
+  def initialize(venue, date, price)
+    @venue = venue
+    @date = date
+    @price = price
+  end
+  #etc
+  def price
+    @price
+  end
+  #etc.
+end
+th = Ticket.new("Town Hall", "2023-05-29", 63.00)
+###############################
+class Ticket
+  def initialize(venue, date)
+    @venue = venue
+    @date = date
+  end
+
+  def set_price(amount)
+    @price = amount
+  end
+
+  def price
+    @price
+  end
+end
+ticket = Ticket.new("Town Hall", "2023-06-23")
+ticket.set_price(63.00)
+puts "The ticket costs $#{"%.2f" % ticket.price}."
+ticket.set_price(72.50)
+puts "Whoops -- it just went up. It now costs $#{"%.2f" % ticket.price}."
+
+# Syntactic sugar for assignment-like methods
+# ticket.price(63.00) or ticket.price = 63.00
+
+# Setter methods unleashed
+class Silly
+  def price=(x)
+    puts "The current time is #{Time.now}"
+  end
+end
+s = Silly.new
+s.price = 111.22
+
+class Ticket
+  def price=(amount)
+    if (amount * 100).to_i == amount * 100
+      @price = amount
+    else
+      puts "The price seems to be malformed"
+    end
+  end
+  def price
+    @price
+  end
+end
+
+class TravelAgentSession
+  def year=(y)
+    @year = y.to_i
+    if @year < 100
+      @year = @year + 2000
+    end
+  end
+end
+
+# Exercises
+# 1
+class Ticket 
+  def initialize(venue, price)
+    @venue = venue
+    @price = price
+  end
+
+  def venue
+    @venue
+  end
+  puts "hola"
+  def discount=(disc)
+    @discount = @price - (@price * (disc/100))
+    puts "Descuento"
+  end
+end
+tk = Ticket.new("Town Hall", 100)
+puts "The ticket for #{tk.venue} has been discounted 15% to #{tk.discount(15)}"
+
+# Automating the creation of attributes
+# Listing 3.2 Ticket class, with the attribute
+# reader/writer methods spelled out
+class Ticket
+  def initialize(venue, date)
+    @venue = venue
+    @date = date
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def venue
+    @venue
+  end
+
+  def date
+    @date
+  end
+
+  def price
+    @price
+  end
+end
+
+# Listing 3.3 Ticket class, with getter and setter methods
+# defined via attr_* calls
+class Ticket
+  attr_reader :venue, :date, :price
+  attr_writer :price
+  
+  def initialize(venue, date)
+    @venue = venue
+    @date = date
+  end
+end
