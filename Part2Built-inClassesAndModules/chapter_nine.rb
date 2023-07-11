a3b5c599ab591593599f4a277ed6aed3687c07d8
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2023-07-11 14:09:28 -0600

    Primeros ejercicios del capitulo 9

--- Part2Built-inClassesAndModules/chapter_nine.rb
+++ Part2Built-inClassesAndModules/chapter_nine.rb
@@ -1,3 +1,29 @@
 # Chapter 9
 # Collection and
 # container objects
+
+# Creating a new array
+# Array.new
+a = Array.new
+puts "#{a}"
+
+puts "#{Array.new(3)}"
+puts "#{Array.new(3, "abc")}"
+
+# The literal array constructor []
+a = []
+puts a
+a = [1,2, "three", 4, []]
+puts "#{a}"
+
+# The %w and %W array constructor
+puts "#{%w(Hector Manuel VIII)}"
+# or
+puts "#{%w{Hector Manuel VIII}}"
+#
+puts "#{%W(Hector is #{2023 - 1998} years old)}"
+
+# The %i and %I array constructors
+puts "#{%i(a b c)}"
+h = "Hector"
+puts "#{%I("#{h}")}"
