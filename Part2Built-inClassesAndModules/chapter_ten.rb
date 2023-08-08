0bd01827c70812eef2879fb3941f3ad2c0ef5345
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2023-08-08 15:32:45 -0600

    Comenzar hacer ejercicios del capitulo 10

--- Part2Built-inClassesAndModules/chapter_ten.rb
+++ Part2Built-inClassesAndModules/chapter_ten.rb
@@ -2,3 +2,23 @@
 # Collection central:
 # Enumerable and Enumerator
 
+# Gaining enumerability throgh each
+# Listing 10.1 An Enumerable class and its deployment of the each method
+
+class Rainbow
+  include Enumerable
+
+  def each
+    yield "red"
+    yield "orange"
+    yield "green"
+    yield "blue"
+    yield "indigo"
+    yield "violet"
+  end
+end
+
+r = Rainbow.new
+r.each do |color|
+  puts "Next color: #{color}"
+end
